cb5e430a38ca6b5b3b6efbc65b511b9d48644ddb
ProsoftDevelopers <44056356+ProsoftDevelopers@users.noreply.github.com>
2018-10-15 15:12:43 +0530

    updated

--- trazer_login.php
+++ trazer_login.php
@@ -1,24 +1,20 @@
 <?php 
-	//echo "validateuser.php"; die();
-// 	$mysql_host='localhost';
-// 	$mysql_user='prosofte_trazer';
-// 	$mysql_password='trazer@2018';	
 
-// 	$con = mysqli_connect($mysql_host,$mysql_user,$mysql_password,"prosofte_trazer");
+$mobile = $_POST['mobile'];  //echo 'hello'.$mobile; die();
+$device_no = $_POST['device_no'];
 
-    $mysql_host='35.200.241.253';
-	$mysql_user='root';
-	$mysql_password='12345';
-
-	$con = mysqli_connect($mysql_host,$mysql_user,$mysql_password,"C5Cell_Tracker");
-
+$serverName = "tcp:prosoftserver.database.windows.net,1433";
+$connectionOptions = array(
+    "Database" => "C5Cell_Tracker",
+    "Uid" => "celltracker",
+    "PWD" => "Prosoftdev12#"
+);
+//Establishes the connection
+$conn = sqlsrv_connect($serverName, $connectionOptions);
 	
-	$mobile = $_POST['mobile'];  //echo 'hello'.$mobile; die();
-	$device_no = $_POST['device_no'];
-	
-	$validatemobile_sql = "SELECT * FROM users_table WHERE mobile='".$mobile."' "; // AND device_no='".$device_no."'
-	$validatemobile_res = mysqli_query($con, $validatemobile_sql); // echo $validatemobile_sql; die();
-	$cnt = $validatemobile_res->num_rows; 
+$validatemobile_sql = "SELECT * FROM users_table WHERE mobile='".$mobile."' "; // AND device_no='".$device_no."'
+$validatemobile_res = mysqli_query($con, $validatemobile_sql); // echo $validatemobile_sql; die();
+$cnt = $validatemobile_res->num_rows; 
 	if($cnt > 0)
 	{
 			$validatedevice_sql = "SELECT * FROM users_table WHERE mobile='".$mobile."' AND device_no='".$device_no."' ";
@@ -42,62 +38,14 @@
 					$ret['status'] = $otp_number; 
     					    	
     			    		$mob_number = $ret['mobile'];
-    					    
-    					   //print_r($ret); //return;
-    					 	
-    		        //$fullapiurl="http://smsc.biz/httpapi/send?username=inamullah.mulla@gmail.com&password=Password1&sender_id=PROMOTIONAL&route=P&phonenumber=$mob_number&message=$enc_msg";
-    		        
-    		        //	$fullapiurl= "https://2factor.in/API/V1/16b43aa4-916d-11e8-a895-0200cd936042/SMS/".$mob_number."/".$otp_number."";
-    		        	
-    		        	//echo $fullapiurl; //die();
-				
-					//echo $fullapiurl; die();
+    					
 					//Call API
 					$ch = curl_init($fullapiurl);
 					curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 					$result = curl_exec($ch); 
 					//echo $result ; // For Report or Code Check
 					curl_close($ch);	
-    					 	
-    					 	
-    					 	// old function starts
-    					 /* $mobileNumber = urlencode($ret['mobile']);
-		                $message = urlencode($message_content);		
-					$url = 'http://smsc.biz/httpapi/send?username=inamullah.mulla@gmail.com&password=Password1&sender_id=PROMOTIONAL&route=P&phonenumber='.$mobileNumber.'&message='.$message.'';
-                
-					 //echo "url = ".$url; die();
-						 $ch = curl_init();
-					         curl_setopt_array($ch, array(
-						 CURLOPT_URL => $url,
-						 CURLOPT_RETURNTRANSFER => true,
-						CURLOPT_POST => true
-					//,CURLOPT_POSTFIELDS => $postData
-							//,CURLOPT_FOLLOWLOCATION => true
-					));
-					
-					curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-					curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-					//echo "<h2>Ignoring certificate...<h2>";
-					
-					//get response
-					//echo "<h2>Curl executing...<h2>";
-					$output = curl_exec($ch);
-					$info = curl_getinfo($ch);
-					//echo "<h2>"; print_r($info); echo "<h2>";
-							
-					
-					//Print error if any
-					if (curl_errno($ch)) 
-					{
-						//$messageStatus .= "<h2>Curl not executed.........<h2>";
-						$messageStatus .= "Error...= ". curl_error($ch)."<br/>";
-						$messageStatus .= "<h4>Message = ". $output. "<h4><br/>";
-						
-					}
-					
-					curl_close($ch); */
-							
-								
+    					 								
 			
 			}
 			}else
