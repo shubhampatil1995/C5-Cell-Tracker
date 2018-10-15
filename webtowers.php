40bc57af72b4489f1931b0b28191f499a5f24e88
ProsoftDevelopers <44056356+ProsoftDevelopers@users.noreply.github.com>
2018-10-15 11:25:46 +0530

    query issue fixed

--- webtowers.php
+++ webtowers.php
@@ -12,7 +12,8 @@ $connectionOptions = array(
 
 //Establishes the connection
 $conn = sqlsrv_connect($serverName, $connectionOptions);
-$tsql = "SELECT [TowerID_Original],[TowerName],[Latitude],[Longitude],[Address1],[Address2],[TowerCity],[TowerState],[TowerCountry],[Azimuth],[ServiceProviderName] from towers_details m wheregeometry::STGeomFromText('Polygon((".$tower_id."))',4326).STIntersects(Geopoints.MakeValid())=1";
+$tsql = "SELECT [TowerID_Original],[TowerName],[Latitude],[Longitude],[Address1],[Address2],[TowerCity],[TowerState],[TowerCountry],[Azimuth],
+[ServiceProviderName] from towers_details m where geometry::STGeomFromText('Polygon((".$tower_id."))',4326).STIntersects(Geopoints.MakeValid())=1";
 
 $getResults= sqlsrv_query($conn, $tsql);
 $ret=NULL; $c=0;
