9e0a4f0eb0e233e24b24c996e4bd224bedf2eda5
ProsoftDevelopers <44056356+ProsoftDevelopers@users.noreply.github.com>
2018-10-15 12:03:51 +0530

    q2

--- fetch_towersdetails.php
+++ fetch_towersdetails.php
@@ -13,8 +13,9 @@ $connectionOptions = array(
 //Establishes the connection
 $conn = sqlsrv_connect($serverName, $connectionOptions);
 
-$tsql = "SELECT [TowerID_Original],[TowerName],[Latitude],[Longitude],[Address1] ,[Address2],[TowerCity] as City,[TowerState] as [State],[TowerCountry] as Country,[Azimuth],[ServiceProviderName]
-FROM towers_details WITH(NOLOCK) WHERE(ISNUMERIC(Latitude) = 1) AND (ISNUMERIC(Longitude) = 1) AND (6378* sqrt(POWER(Radians(Convert(float,".$latitude."))- Radians(Convert(float,Latitude)),2)+ POWER(Radians(Convert(float,".$longitude."))-Radians(Convert(float,Longitude)),2))<=".$radius.")";
+$tsql = "SELECT [TowerID_Original],[TowerName],[Latitude],[Longitude],[Address1] ,[Address2],[TowerCity] as City,[TowerState] as [State],
+[TowerCountry] as Country,[Azimuth],[ServiceProviderName] FROM towers_details WITH(NOLOCK) 
+WHERE(ISNUMERIC(Latitude) = 1) AND (ISNUMERIC(Longitude) = 1) AND (6378* sqrt(POWER(Radians(Convert(float,".$latitude.")) - Radians(Convert(float,Latitude)),2)+ POWER(Radians(Convert(float,".$longitude."))-Radians(Convert(float,Longitude)),2))<=".$radius.")";
 
  echo $tsql;die();
  
