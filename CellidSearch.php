af18a663a797980e5eb4ceb36a2a0c0d525a4678
ProsoftDevelopers <44056356+ProsoftDevelopers@users.noreply.github.com>
2018-10-15 13:15:08 +0530

    updating code from new DB

--- CellidSearch.php
+++ CellidSearch.php
@@ -1,36 +1,34 @@
 <?php 
 
- 	//$mysql_host='localhost';
- 	//$mysql_user='prosofte_trazer';
- //	$mysql_password='trazer@2018';
+$serverName = "tcp:prosoftserver.database.windows.net,1433";
+$connectionOptions = array(
+    "Database" => "C5Cell_Tracker",
+    "Uid" => "celltracker",
+    "PWD" => "Prosoftdev12#"
+);
 
-    $mysql_host='35.200.241.253';
-	$mysql_user='root';
-	$mysql_password='12345';
-
-	$con = mysqli_connect($mysql_host,$mysql_user,$mysql_password,"C5Cell_Tracker");
-//	print_r($con); die();
+//Establishes the connection
+$conn = sqlsrv_connect($serverName, $connectionOptions);
 	
 	$towerid= $_POST["cell_id"];
 	$array_towerids= explode(',', $towerid);
 
-
 	$sql_delete = "DELETE FROM temp_towerdetails";
-	$res_delete = mysqli_query($con,$sql_delete);
+	$res_delete = sqlsrv_query($conn,$sql_delete);
 	
 	//404-49-58002-100
 	//towerid_original
 	for($i=0; $i<count($array_towerids); $i++)
 	{
 		$sql = "SELECT DISTINCT * FROM towers_details WHERE towerid_original LIKE '%".$array_towerids[$i]."%' ";
-		$res = mysqli_query($con, $sql); //print_r($res); die();//echo $sql; die()
-		$cnt = $res->num_rows;
+		$getResults = sqlsrv_query($conn, $sql); //print_r($res); die();//echo $sql; die()
+		$cnt = $getResults->num_rows;
 		$c=0;$ret=NULL;
 		if($cnt > 0)
 		{
-			while($row=mysqli_fetch_assoc($res))
+			while($row=sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC))
 			{
-				// $ret[$c]['sptowersid'] = $row['sptowersid'];
+		
 				$ret[$c]['spmasterid'] = $row['spmasterid'];
 				$ret[$c]['spcirclemasterid'] = $row['spcirclemasterid'];
 				$ret[$c]['towerid'] = $row['towerid'];
@@ -54,59 +52,38 @@
 				$ret[$c]['spnameid'] = $row['spnameid'];
 				$ret[$c]['cellid'] = $row['cellid'];
 				$ret[$c]['place'] = $row['place'];
-				$ret[$c]['cgi'] = $row['cgi'];
-				$ret[$c]['lac_cellid'] = $row['lac_cellid'];
-				
-				//print_r($ret); die();
-				// $sql_insert = "INSERT INTO temp_towerdetails(tsptowersid, tspmasterid, tspcirclemasterid, ttowerid, ttowerid_original, ttowername, ttowerName_original, tlatitude, tlongitude, tgeopoints, taddress1, taddress2, ttowercity, ttowerstate, ttowercountry, tazimuth, tmsc_name, tmcc, tmnc, tlac, trequiredzeros, tspnameid, tcellid, tplace, tcgi, tlac_cellid) VALUES
-				// ('".$ret[$c]['sptowersid']."', '".$ret[$c]['spmasterid']."', '".$ret[$c]['spcirclemasterid']."', '".$ret[$c]['towerid']."', '".$ret[$c]['towerid_original']."', '".$ret[$c]['towername']."', '".$ret[$c]['towerName_original']."', '".$ret[$c]['latitude']."', '".$ret[$c]['longitude']."', '".$ret[$c]['geopoints']."', '".$ret[$c]['address1']."', '".$ret[$c]['address2']."', '".$ret[$c]['towercity']."', '".$ret[$c]['towerstate']."', '".$ret[$c]['towercountry']."', '".$ret[$c]['azimuth']."', '".$ret[$c]['msc_name']."', '".$ret[$c]['mcc']."', '".$ret[$c]['mnc']."', '".$ret[$c]['lac']."', '".$ret[$c]['requiredzeros']."', '".$ret[$c]['spnameid']."', '".$ret[$c]['cellid']."', '".$ret[$c]['place']."', '".$ret[$c]['cgi']."', '".$ret[$c]['lac_cellid']."')";
-				
-				 $sql_insert = "INSERT INTO temp_towerdetails(tspmasterid, tspcirclemasterid, ttowerid, ttowerid_original, ttowername, ttowerName_original, tlatitude, tlongitude, tgeopoints, taddress1, taddress2, ttowercity, ttowerstate, ttowercountry, tazimuth, tmsc_name, tmcc, tmnc, tlac, trequiredzeros, tspnameid, tcellid, tplace, tcgi, tlac_cellid) VALUES
-				 ('".$ret[$c]['spmasterid']."', '".$ret[$c]['spcirclemasterid']."', '".$ret[$c]['towerid']."', '".$ret[$c]['towerid_original']."', '".$ret[$c]['towername']."', '".$ret[$c]['towerName_original']."', '".$ret[$c]['latitude']."', '".$ret[$c]['longitude']."', '".$ret[$c]['geopoints']."', '".$ret[$c]['address1']."', '".$ret[$c]['address2']."', '".$ret[$c]['towercity']."', '".$ret[$c]['towerstate']."', '".$ret[$c]['towercountry']."', '".$ret[$c]['azimuth']."', '".$ret[$c]['msc_name']."', '".$ret[$c]['mcc']."', '".$ret[$c]['mnc']."', '".$ret[$c]['lac']."', '".$ret[$c]['requiredzeros']."', '".$ret[$c]['spnameid']."', '".$ret[$c]['cellid']."', '".$ret[$c]['place']."', '".$ret[$c]['cgi']."', '".$ret[$c]['lac_cellid']."')";
 				
 				
-				$res_insert = mysqli_query($con,$sql_insert); //echo $sql_insert; die();
+				 $sql_insert = "INSERT INTO temp_towerdetails(tspmasterid, tspcirclemasterid, ttowerid, ttowerid_original, ttowername, ttowerName_original, tlatitude, tlongitude, tgeopoints, taddress1, taddress2, ttowercity, ttowerstate, ttowercountry, tazimuth, tmsc_name, tmcc, tmnc, tlac, trequiredzeros, tserviceprovidername, tcellid, tplace) VALUES
+				 ('".$ret[$c]['spmasterid']."', '".$ret[$c]['spcirclemasterid']."', '".$ret[$c]['towerid']."', '".$ret[$c]['towerid_original']."', '".$ret[$c]['towername']."', '".$ret[$c]['towerName_original']."', '".$ret[$c]['latitude']."', '".$ret[$c]['longitude']."', '".$ret[$c]['geopoints']."', '".$ret[$c]['address1']."', '".$ret[$c]['address2']."', '".$ret[$c]['towercity']."', '".$ret[$c]['towerstate']."', '".$ret[$c]['towercountry']."', '".$ret[$c]['azimuth']."', '".$ret[$c]['msc_name']."', '".$ret[$c]['mcc']."', '".$ret[$c]['mnc']."', '".$ret[$c]['lac']."', '".$ret[$c]['requiredzeros']."', '".$ret[$c]['serviceprovidername']."', '".$ret[$c]['cellid']."', '".$ret[$c]['place']."')";
+				
+				
+				$res_insert = sqlsrv_query($conn,$sql_insert); //echo $sql_insert; die();
 				
 				$c++; 
 			}//die();
 		}
 		
 		$sql_fetch = "SELECT * FROM temp_towerdetails";
-		$res_fetch = mysqli_query($con, $sql_fetch);//  echo $sql_fetch; die();
+		$res_fetch = sqlsrv_query($conn, $sql_fetch);//  echo $sql_fetch; die();
 		$cnt_fetch = $res_fetch->num_rows;
 		$ret1=NULL; $i=0;  
 		if($cnt_fetch > 0)
 		{
-    		while($row1=mysqli_fetch_assoc($res_fetch))
+    		while($row1=sqlsrv_fetch_array($res_fetch, SQLSRV_FETCH_ASSOC))
     		{
-    			//$ret1[$i]['temp_towerid'] = $row1['temp_towerid'];
-    // 			$ret1[$i]['sptowersid'] = $row1['tsptowersid'];
-    // 			$ret1[$i]['spmasterid'] = $row1['tspmasterid'];
-    // 			$ret1[$i]['spcirclemasterid'] = $row1['tspcirclemasterid'];
-    // 			$ret1[$i]['towerid'] = $row1['ttowerid'];
+
     			$ret1[$i]['towerid_original'] = $row1['ttowerid_original'];
     			$ret1[$i]['towername'] = $row1['ttowername'];
-    // 			$ret1[$i]['towerName_original'] = $row1['ttowerName_original'];
     			$ret1[$i]['latitude'] = $row1['tlatitude'];
     			$ret1[$i]['longitude'] = $row1['tlongitude'];
-    // 			$ret1[$i]['geopoints'] = $row1['tgeopoints'];
     			$ret1[$i]['address1'] = $row1['taddress1'];
     			$ret1[$i]['address2'] = $row1['taddress2'];
     			$ret1[$i]['towercity'] = $row1['ttowercity'];
     			$ret1[$i]['towerstate'] = $row1['ttowerstate'];
     			$ret1[$i]['towercountry'] = $row1['ttowercountry'];
     			$ret1[$i]['azimuth'] = $row1['tazimuth'];
-    // 			$ret1[$i]['msc_name'] = $row1['tmsc_name'];
-    // 			$ret1[$i]['mcc'] = $row1['tmcc'];
-    // 			$ret1[$i]['mnc'] = $row1['tmnc'];
-    // 			$ret1[$i]['lac'] = $row1['tlac'];
-    // 			$ret1[$i]['requiredzeros'] = $row1['trequiredzeros'];
     			$ret1[$i]['spnameid'] = $row1['tspnameid'];
-    // 			$ret1[$i]['cellid'] = $row1['tcellid'];
-    // 			$ret1[$i]['place'] = $row1['tplace'];
-    // 			$ret1[$i]['cgi'] = $row1['tcgi'];
-    // 			$ret1[$i]['lac_cellid'] = $row1['tlac_cellid'];
-    			
     			$i++;
     		}
 		}
@@ -114,4 +91,4 @@
 	
 	$user_data = json_encode($ret1); //print_r($user_data); die();
 	print $user_data;
-?>	
+?>
